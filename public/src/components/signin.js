53e362962b2fb3b59bac999d6fcd2f7537cb19e9
Absar Ul Bashir <techarduino57@gmail.com>
2023-02-16 22:51:15 +0530

    added toastify

--- public/src/components/signin.js
+++ public/src/components/signin.js
@@ -1,21 +1,40 @@
-import { useContext, useState, useEffect } from "react";
+import React,{ useContext, useState, useEffect } from "react";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { Context } from "./context";
+import Logo from "../images/logo.jpg";
+
 function Signin() {
   const context = useContext(Context);
   const [username, setUsername] = useState();
   const [password, setPassword] = useState();
   const [avatar, setAvatar] = useState();
-
+  
   useEffect(() => {
     setAvatar(avatar);
   }, [avatar]);
 
+  const signin=()=>{
+    toast("Hello how are you",{
+      position: toast.POSITION.BOTTOM_RIGHT,
+      autoClose: 3000,
+      style:{color:"white",backgroundColor:"indigo"}
+    })
+  }
+
   return (
     <>
       <div id="signContainer">
+        <div id="signLogo">
+          <img src={Logo} alt="err" />
+        </div>
         <div id="signin">
           <label htmlFor="upload">
-            <img src="https://media.istockphoto.com/id/1273297997/vector/default-avatar-profile-icon-grey-photo-placeholder-hand-drawn-modern-man-avatar-profile-icon.jpg?s=170667a&w=0&k=20&c=JEH1FikFgnjjC__PjHbbO0h7oFMlodNfoKF6ANBgP8o=" alt="" id="profile" />
+            <img
+              src="https://media.istockphoto.com/id/1273297997/vector/default-avatar-profile-icon-grey-photo-placeholder-hand-drawn-modern-man-avatar-profile-icon.jpg?s=170667a&w=0&k=20&c=JEH1FikFgnjjC__PjHbbO0h7oFMlodNfoKF6ANBgP8o="
+              alt=""
+              id="profile"
+            />
             <span class="material-symbols-outlined">add</span>
           </label>
           <input
@@ -28,28 +47,31 @@ function Signin() {
               reader.readAsDataURL(file);
               reader.addEventListener("load", () => {
                 setAvatar(reader.result);
-                let img=document.querySelector("#profile");
-                img.src=reader.result;
+                let img = document.querySelector("#profile");
+                img.src = reader.result;
               });
             }}
           />
           <input
             type="text"
+            placeholder="Enter a username"
             onChange={(e) => {
               setUsername(e.target.value);
             }}
           />
           <input
             type="password"
+            placeholder="Enter your password"
             onChange={(e) => {
               setPassword(e.target.value);
             }}
           />
-          <button id="signSubmit">Submit</button>
+          <button id="signSubmit" onClick={signin}>Submit</button>
           <p>Already have an account</p>
           <a href="">Login</a>
         </div>
       </div>
+      <ToastContainer/>
     </>
   );
 }
