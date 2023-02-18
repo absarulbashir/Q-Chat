e1d9dae0daac86e3dc16c5eec5d03f24fd6cfbc4
Absar Ul Bashir <techarduino57@gmail.com>
2023-02-18 16:41:23 +0530

    added multiavator api

--- public/src/components/context.js
+++ public/src/components/context.js
@@ -5,8 +5,31 @@ const Context = createContext();
 
 export default function contextApi(props){
 
+    const signin = async (username,password,avatar)=>{
+        
+        const response = await fetch("http://127.0.0.1:5000/api/auth/signin", {
+            method: "POST",
+            headers: {
+              'Content-Type': 'application/json',
+              'Content-Length': 1
+            },
+
+            body: JSON.stringify({
+                username,
+                password,
+                messages:{},
+                avatar,
+            })
+          });
+        
+          const res = await response.json();
+          console.log(res);
+    }
+
     return(
-        <Context.Provider value="">
+        <Context.Provider value={{
+            signin
+        }}>
             {props.children}
         </Context.Provider>
     )
--- public/src/components/signin.js
+++ public/src/components/signin.js
@@ -3,55 +3,58 @@ import { ToastContainer, toast } from 'react-toastify';
 import ToastCss from 'react-toastify/dist/ReactToastify.css';
 import { Context } from "./context";
 
-console.log(ToastCss)
 
 function Signin() {
   const context = useContext(Context);
-  const [username, setUsername] = useState();
-  const [password, setPassword] = useState();
-  const [avatar, setAvatar] = useState();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [avatar, setAvatar] = useState("");
   
   useEffect(() => {
     setAvatar(avatar);
   }, [avatar]);
 
   const signin=()=>{
-    toast("Hello how are you",{
-      position: toast.POSITION.BOTTOM_RIGHT,
-      autoClose: 3000,
-      style:{color:"white",backgroundColor:"rgb(119, 4, 71)",fontSize:"larger",fontFamily:"poppins"}
-    })
+    let userSPace = username.indexOf(" ");
+    let passSpace = password.indexOf(" ");
+    if(username.length<1 || password.length<=5 || avatar.length<=32){
+      toast("PLease fill in the credentials",{
+        position:toast.POSITION.BOTTOM_RIGHT,
+        style:{background:"brown",color:"white",fontSize:"larger",fontFamily:"poppins"}
+      });
+    }
+    else if(userSPace>=0 || passSpace>=0){
+      toast("Cannot include spaces",{
+        position:toast.POSITION.BOTTOM_RIGHT,
+        style:{background:"brown",color:"white",fontSize:"larger",fontFamily:"poppins"}
+      });
+    }
+    else{
+      context.signin(username,password,avatar)
+    }
   }
 
   return (
     <>
       <div id="signContainer">
-
         <div id="signin">
           <label htmlFor="upload">
             <img
-              src="https://media.istockphoto.com/id/1273297997/vector/default-avatar-profile-icon-grey-photo-placeholder-hand-drawn-modern-man-avatar-profile-icon.jpg?s=170667a&w=0&k=20&c=JEH1FikFgnjjC__PjHbbO0h7oFMlodNfoKF6ANBgP8o="
+              src="https://api.multiavatar.com/1.png"
               alt=""
               id="profile"
             />
           </label>
-          <input
-            id="upload"
-            type="file"
-            accept="jpg/png"
-            onChange={(event) => {
-              const file = event.target.files[0];
-              const reader = new FileReader();
-              reader.readAsDataURL(file);
-              reader.addEventListener("load", () => {
-                setAvatar(reader.result);
-                let img = document.querySelector("#profile");
-                img.src = reader.result;
-              });
-            }}
-          />
+          <input type="text" id="avatar" placeholder="Enter random name" onChange={async(e)=>{
+            const res = await fetch(`https://api.multiavatar.com/${e.target.value}.png`);
+            const img = await res.JSON;
+            let profile = document.querySelector("#profile");
+            profile.src=res.url;
+            setAvatar(`https://api.multiavatar.com/${e.target.value}.png`)
+          }} />
           <input
             type="text"
+            id="username"
             placeholder="Enter a username"
             onChange={(e) => {
               setUsername(e.target.value);
@@ -64,7 +67,7 @@ function Signin() {
               setPassword(e.target.value);
             }}
           />
-          <button id="signSubmit" onClick={signin}>Submit</button>
+          <button id="signSubmit" onClick={signin}>Signin</button>
           <p>Already have an account</p>
           <a href="">Login</a>
         </div>
